d1dac305610315080e941f6cb34f58104d59d965
Michal Czudziak <michalczudziak@gmail.com>
2025-08-19 09:36:19 +0200

    Added first accepted solution - very slow!

---
+++ easy/349-intersection_of_two_arrays.js
@@ -0,0 +1,32 @@
+var intersection = function (nums1, nums2) {
+  const binarySearch = (nums, target) => {
+    let left = 0;
+    let right = nums.length - 1;
+    let middle = 0;
+    while (left <= right) {
+      middle = Math.floor((left + right) / 2);
+      if (nums[middle] === target) {
+        return middle;
+      } else if (nums[middle] > target) {
+        right = middle - 1;
+      } else {
+        left = middle + 1;
+      }
+    }
+    return -1;
+  };
+
+  nums1.sort((a, b) => a - b);
+  nums2.sort((a, b) => a - b);
+  let returnNums = [];
+  for (let i = 0; i < nums1.length; i++) {
+    if (binarySearch(nums2, nums1[i]) !== -1) {
+      if (binarySearch(returnNums, nums1[i]) === -1) {
+        returnNums.push(nums1[i]);
+        returnNums.sort((a, b) => a - b);
+      }
+    }
+  }
+
+  return returnNums;
+};
